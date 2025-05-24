12597d28c0843a6b0186f130960e84cc1230f1ac
Jalaj2005 <jalajsingh2005@gmail.com>
2025-05-24 21:33:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def preprocess_image(image_path):
     img = Image.open(image_path).convert('L')  # Convert to grayscale
     img = img.resize((28, 28))
     img = np.array(img)
-    img = 255 - img  # Invert colors (white digit on black bg)
+    img = 255 - img  # Invert colors
     img = img / 255.0  # Normalize
     img = img.reshape(1, 28, 28, 1)  # Add batch and channel dimensions
     return img
@@ -51,7 +51,7 @@ def index():
 
         image = preprocess_image(filepath)
         pred = model.predict(image)
-        prediction = np.argmax(pred)
+        prediction = int(np.argmax(pred))
         image_url = file.filename
 
     return render_template('index.html', prediction=prediction, image_url=image_url)
@@ -61,5 +61,6 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
